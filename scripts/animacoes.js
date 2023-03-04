2179d84da2e926bceb45843ac86fe8589db2e202
Matheus Pereira de Souza <109758729+desenvolvedormatheus@users.noreply.github.com>
2023-03-04 20:12:34 -0300

    1.0
    Responsividade,
    Escolha de cores do usuário,
    coreção de bugs

--- scripts/animacoes.js
+++ scripts/animacoes.js
@@ -1,8 +1,6 @@
 // Modo Noturno
 // Notebook
-
 const radio = document.querySelector('.radio');
-
 radio.addEventListener('click', function() {
     const lapwhite = document.querySelector('.lapwhite')
     const lapblack = document.querySelector('.lapblack')
@@ -14,24 +12,32 @@ radio.addEventListener('click', function() {
         btn.style.left = ativo;
         document.documentElement.style.setProperty('--fundo', '#000');
         document.documentElement.style.setProperty('--texto', '#fff');
-        lapblack.style.display = 'none'
-        lapwhite.style.display = 'initial'
+        if(window.innerWidth > 800){
+            lapblack.style.display = 'none'
+            lapwhite.style.display = 'initial'
+        }
     } else {
         btn.style.left = inativo;
         document.documentElement.style.setProperty('--fundo', '#fff');
         document.documentElement.style.setProperty('--texto', '#000');
-        lapwhite.style.display = 'none'
-        lapblack.style.display = 'initial'
+        if(window.innerWidth > 800){
+            lapwhite.style.display = 'none'
+            lapblack.style.display = 'initial'
+        }
     };
 });
-
+// Btn segundaria do do site
+const aplicarCor = document.querySelector('.aplicarCor')
+aplicarCor.addEventListener('click', function(){
+    const caixaCor = document.querySelector('#cor').value
+    document.documentElement.style.setProperty('--segundaria', caixaCor)
+    alert('Algumas cores como preto, branco e cores claras podem não dar um bom contraste, mas fica ao seu critério')
+})
 // Btn para voltar ao topo
 const seta = document.querySelector('.bi-arrow-up')
-
 seta.addEventListener('click', function(){
         window.scrollTo(0, 0);
 })
-
 function conferirseta(){
     let scroll = window.pageYOffset;
     if(scroll === 0){
@@ -41,3 +47,16 @@ function conferirseta(){
     }
 }
 setInterval(conferirseta, 100)
+
+// Menu max-width 680px
+const menu = document.querySelector('.haburguer')
+menu.addEventListener('click', function(){
+    const ul = document.querySelector('ul')
+    ul.style.top = '6rem'
+});
+const fecharMenu = document.querySelector('.fechar')
+
+fecharMenu.addEventListener('click', function(){  
+    const ul = document.querySelector('ul')
+    ul.style.top = '-30rem'
+})
