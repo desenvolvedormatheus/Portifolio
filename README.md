fb42b88def2a375a35725b5e39c957236703802f
Matheus Pereira de Souza <109758729+desenvolvedormatheus@users.noreply.github.com>
2023-05-05 15:22:55 -0300

    2.0
    Portifolio totalmente refeito.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portifolio
- meu portifolio pessoal
---
+++ index.js
@@ -0,0 +1,38 @@
+// Modo noturno | Claro
+document.querySelector(".btnNoturno").addEventListener("click", ()=>{
+    if (document.documentElement.style.getPropertyValue('--fundoperfil') != '#fff'){
+        document.querySelector(".sol").style.display = "none"
+        document.querySelector(".displayN").style.display = "initial"
+        document.documentElement.style.setProperty('--fundoperfil', '#fff');
+        document.documentElement.style.setProperty('--letras', '#0a0a0a');
+        document.querySelector(".lapblack").style.display = "flex"
+        document.querySelector(".lapwhite").style.display = "none"
+        
+    }else{
+        document.querySelector(".sol").style.display = "initial"
+        document.querySelector(".displayN").style.display = "none"
+        document.documentElement.style.setProperty('--fundoperfil', '#111111');
+        document.documentElement.style.setProperty('--letras', '#fff');
+        document.querySelector(".lapblack").style.display = "none"
+        document.querySelector(".lapwhite").style.display = "flex"
+    }
+})
+
+// Menu lateral
+document.querySelector(".btnMenu").addEventListener("click", ()=>{
+    if (getComputedStyle(document.querySelector(".lateralE")).getPropertyValue("width") == "48px"){
+        document.querySelector(".lateralE").style.width = "17rem"
+        // document.querySelector(".centro").style.marginLeft = "17rem"
+        document.querySelector(".menu").style.paddingRight = "0rem"
+    } else{
+        document.querySelector(".lateralE").style.width = "4rem"
+        // document.querySelector(".centro").style.marginLeft = "4rem"
+        document.querySelector(".menu").style.paddingRight = "15rem"
+    }
+})
+
+// Carregamento da pagina
+window.onload = ()=>{
+    document.querySelector("main").style.opacity = "1"
+    document.querySelector(".carregando").style.display = "none"
+}
